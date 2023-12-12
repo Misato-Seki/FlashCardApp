7ef2d6e334c0e25aceb4cfb5dcaa0399a3629929
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2023-12-12 10:08:26 +0200

    Add ver2.0_C#

--- ver2.0_C#/Program.cs
+++ ver2.0_C#/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿class QuizProgram
+{
+    static void Main()
+    {
+        //create questions
+        string[] questions =
+        {
+            "dog",
+            "cat",
+            "rabbit"
+        };
+
+        //create answers
+        string[] answeres =
+        {
+            "A. koira\nB. kissa\nC.kani",
+            "A. koira\nB. kissa\nC.kani",
+            "A. koira\nB. kissa\nC.kani"
+        };
+
+        //create correctAnsweres
+        int[] correctAnswers ={0, 1, 2};
+
+        //recod players score
+        int playersScore = 0;
+
+        //ask questions
+        Console.WriteLine("Welcome to the Finnish flashcard app!");
+        for(int i = 0; i < questions.Length; i++)
+        {
+            Console.WriteLine("Question " + (i + 1));
+            Console.WriteLine(questions[i]);
+            Console.WriteLine(answeres[i]);
+            Console.Write("Please enter your answer ('A', 'B', or 'C'): ");
+            string playersAnswer = Console.ReadLine();
+
+            //validating answer
+            if(string.Equals(playersAnswer, "A", StringComparison.OrdinalIgnoreCase) && correctAnswers[i] == 0)
+            {
+                playersScore++;
+            }
+            else if(string.Equals(playersAnswer, "B", StringComparison.OrdinalIgnoreCase) && correctAnswers[i] == 1)
+            {
+                playersScore++;
+            }
+            else if(string.Equals(playersAnswer, "C", StringComparison.OrdinalIgnoreCase) && correctAnswers[i] == 2)
+            {
+                playersScore++;
+            }
+
+            //print score out to users
+            Console.WriteLine("Quiz Completed!");
+            Console.WriteLine("Your score is " + playersScore + "/" + questions.Length);
+
+        }
+
+    }
+
+}
